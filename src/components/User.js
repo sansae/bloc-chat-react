7dba82e30a1d7a638b30206d56c09930e9c4764e
Kent <kentsaeteurn@gmail.com>
2018-05-17 09:37:17 -0700

    add sign-out btn that calls firebase's signOut method

--- src/components/User.js
+++ src/components/User.js
@@ -4,17 +4,26 @@ class User extends Component {
 
   signInWithPopup = (e) => {
     e.preventDefault();
-    // console.log('signInWithPopup executed');
 
     const provider = new this.props.firebase.auth.GoogleAuthProvider();
 
     this.props.firebase.auth().signInWithPopup(provider);
   }
 
+  signOut = (e) => {
+    console.log('signOut executed');
+    this.props.firebase.auth().signOut().then(function() {
+      console.log('Signed Out');
+    }, function(error) {
+      console.error('Sign Out Error', error);
+    });
+  }
+
   render() {
     return (
       <div id="btn-div">
-        <button id="user-btn" type="button" onClick={(e) => this.signInWithPopup(e)}>Sign In</button>
+        <button id="sign-in" type="button" onClick={(e) => this.signInWithPopup(e)}>Sign In</button>
+        <button id="sign-out" type="button" onClick={(e) => this.signOut(e)}>Sign Out</button>
       </div>
     )
   }
