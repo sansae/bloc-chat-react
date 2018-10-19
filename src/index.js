6e2876ee6f75b6f2556ce164c83e5e1c404b80a9
Kent <kentsaeteurn@gmail.com>
2018-10-19 07:27:21 -0700

    prevent app from sleeping in production

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const http = require("http");
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+setInterval(() => {
+  http.get("https://message-bored.herokuapp.com/");
+}, 300000);
